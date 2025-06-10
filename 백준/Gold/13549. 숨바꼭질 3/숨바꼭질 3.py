7186cf9bcc56502968618c304f984825322da20d
hongmin dylan lee <107842030+webplusangels@users.noreply.github.com>
2025-06-10 12:08:45 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 136 ms, Memory: 38012 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -3,22 +3,32 @@ from collections import deque
 input = lambda: stdin.readline().rstrip()
 
 N, K = map(int, input().split())
-dq = deque([(N, 0)])
-vis = set([N])
+MAX_POS = 100000
 
-while True:
-    subin, cnt = dq.popleft()
-    if subin == K:
-        print(cnt)
-        break
-        
-    subin_moved = subin*2
-    if 0 <= subin_moved <= 100000 and subin_moved not in vis:
-        vis.add(subin_moved)
-        dq.appendleft((subin_moved, cnt))
+# dist 배열: -1은 미방문, 그 외는 해당 위치까지의 최소 시간
+dist = [-1] * (MAX_POS + 1)
+q = deque()
+
+# 시작점 초기화
+q.append(N)
+dist[N] = 0
+
+while q:
+    curr_pos = q.popleft()
+
+    if curr_pos == K:
+        break # 목표 도달
+
+    # 1. 순간이동 (0초 소요)
+    next_pos = curr_pos * 2
+    if 0 <= next_pos <= MAX_POS and dist[next_pos] == -1: # 아직 방문 안 했다면
+        dist[next_pos] = dist[curr_pos] # 시간 그대로
+        q.appendleft(next_pos) # 우선순위가 높으므로 앞에 추가
     
-    for subin_moved in [subin-1, subin+1]:
-        if 0 <= subin_moved <= 100000 and subin_moved not in vis:
-            vis.add(subin_moved)
-            dq.append((subin_moved, cnt+1))
-    
+    # 2. 걷기 (1초 소요)
+    for next_pos in (curr_pos - 1, curr_pos + 1): # 순서 상관 없음
+        if 0 <= next_pos <= MAX_POS and dist[next_pos] == -1: # 아직 방문 안 했다면
+            dist[next_pos] = dist[curr_pos] + 1 # 시간 + 1
+            q.append(next_pos) # 우선순위가 낮으므로 뒤에 추가
+
+print(dist[K])
