a2dcab6017a6051b77abfd319bccd92575279e9d
hongmin dylan lee <107842030+webplusangels@users.noreply.github.com>
2025-07-08 21:34:16 +0900

    [Silver III] Title: N과 M (6), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,19 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+N, M = map(int, input().split())
+N_list = list(map(int, input().split()))
+
+N_list.sort()
+
+def func(w, l):
+    if len(l) == M:
+        print(*l)
+        return
+
+    for i in range(w, N):
+        l.append(N_list[i])
+        func(i+1, l)
+        l.pop()
+
+func(0, [])
