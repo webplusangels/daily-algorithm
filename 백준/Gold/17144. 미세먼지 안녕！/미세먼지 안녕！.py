4e3e3fa7d98e972fa0af0af09675afd15ade30a3
hongmin dylan lee <107842030+webplusangels@users.noreply.github.com>
2025-08-28 22:43:31 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 368 ms, Memory: 112496 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,40 +1,32 @@
 import sys
-from collections import defaultdict
 input = lambda: sys.stdin.readline().rstrip()
 
 R, C, T = map(int, input().split())
 purify = []
-dusts = {}
 board = []
 for i in range(R):
     tmp = list(map(int, input().split()))
     if tmp[0] == -1:
         purify.append(i)
-    for j in range(C):
-        if tmp[j] > 0:
-            dusts[(i, j)] = tmp[j]
     board.append(tmp)
 
 dx, dy = (0, -1, 0, 1), (1, 0, -1, 0)
 
-def dust_spread():
-    global dusts
-    new_dusts = defaultdict(int)
-    for (x, y), amount in dusts.items():
-        if amount < 5:
-            new_dusts[(x, y)] += amount
-            continue
-        board[x][y] -= amount
-        dusted = amount // 5
-        for n in range(4):
-            xx, yy = x+dx[n], y+dy[n]
-            if 0 <= xx < R and 0 <= yy < C and not board[xx][yy] == -1:
-                amount -= dusted
-                new_dusts[(xx, yy)] += dusted
-                board[xx][yy] += dusted
-        new_dusts[(x, y)] += amount
-        board[x][y] += amount
-    dusts = new_dusts
+def spread():
+    global board
+    new_board = [board[i][:] for i in range(R)]
+
+    for i in range(R):
+        for j in range(C):
+            if board[i][j] >= 5:
+                amount = board[i][j] // 5
+                for n in range(4):
+                    xx, yy = i+dx[n], j+dy[n]
+                    if 0 <= xx < R and 0 <= yy < C and board[xx][yy] != -1:
+                        new_board[xx][yy] += amount
+                        new_board[i][j] -= amount                        
+
+    board = new_board
 
 def purifier():
     u_side, d_side = (purify[0], 1), (purify[1], 1)
@@ -42,10 +34,6 @@ def purifier():
     
     while u_side != (purify[0], 0):
         x, y = u_side
-        if board[x][y]:
-            dusts.pop((x, y))
-        if purified:
-            dusts[(x, y)] = purified
         board[x][y], purified = purified, board[x][y]
         xx, yy = x+dx[u_n], y+dy[u_n]
         if not (0 <= xx < R and 0 <= yy < C):
@@ -56,10 +44,6 @@ def purifier():
     purified = d_n = 0
     while d_side != (purify[1], 0):
         x, y = d_side
-        if board[x][y]:
-            dusts.pop((x, y))
-        if purified:
-            dusts[(x, y)] = purified
         board[x][y], purified = purified, board[x][y]
         xx, yy = x+dx[d_n], y+dy[d_n]
         if not (0 <= xx < R and 0 <= yy < C):
@@ -68,7 +52,7 @@ def purifier():
         d_side = (xx, yy)
 
 for i in range(T):
-    dust_spread()
+    spread()
     purifier()
 
 s = 0
