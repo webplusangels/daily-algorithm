45d6eab7b2a45fc0fb6fe153beb9042675dc3445
hongmin dylan lee <107842030+webplusangels@users.noreply.github.com>
2025-09-08 11:02:47 +0900

    [Silver III] Title: 2×n 타일링, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,14 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+
+dp = [0]*(1000+1)
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+    dp[i] %= 10007
+
+print(dp[n])
