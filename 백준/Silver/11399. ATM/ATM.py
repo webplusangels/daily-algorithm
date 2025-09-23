3d4130dfdc150cb9b8e2f3eae876b9ae0cdf5976
hongmin dylan lee <107842030+webplusangels@users.noreply.github.com>
2025-09-23 10:35:40 +0900

    [Silver IV] Title: ATM, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.py
@@ -0,0 +1,13 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+N = int(input())
+P = list(map(int, input().split()))
+
+P.sort()
+time = 0
+
+for i in range(N):
+    time += P[i]*(N-i)
+    
+print(time)
