a06d083455b9efe52425ab6194c137d570651d03
hongmin dylan lee <107842030+webplusangels@users.noreply.github.com>
2025-11-21 16:56:03 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,13 @@
+def solution(n):
+    if n == 0:
+        print(0)
+        return
+    
+    fib = [0, 1] + [0]*(n-1)
+    for i in range(2, n+1):
+        fib[i] = fib[i-1] + fib[i-2]
+
+    print(fib[n])
+
+N = int(input())
+solution(N)
