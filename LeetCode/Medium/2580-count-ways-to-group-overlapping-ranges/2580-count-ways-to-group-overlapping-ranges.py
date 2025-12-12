f9e7c91cee82282307676faddb528bc0ea7696d5
hongmin dylan lee <107842030+webplusangels@users.noreply.github.com>
2025-12-12 22:59:00 +0900

    Time: 47 ms (35.44%), Space: 46.9 MB (53.88%) - LeetHub

--- LeetCode/Medium/2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.py
+++ LeetCode/Medium/2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.py
@@ -1,24 +1,24 @@
 class Solution:
     def countWays(self, ranges: List[List[int]]) -> int:
         ranges.sort(key=lambda x: (x[0], x[1]))
-        parent = [n for n in range(len(ranges))]
+        # parent = [n for n in range(len(ranges))]
 
-        def find(x):
-            if parent[x] == x:
-                return x
+        # def find(x):
+        #     if parent[x] == x:
+        #         return x
             
-            return find(parent[x])
+        #     return find(parent[x])
         
-        def union(x, y):
-            p_x, p_y = find(x), find(y)
+        # def union(x, y):
+        #     p_x, p_y = find(x), find(y)
 
-            if p_x == p_y:
-                return
+        #     if p_x == p_y:
+        #         return
 
-            if p_x < p_y:
-                parent[p_y] = p_x
-            else:
-                parent[p_x] = p_y
+        #     if p_x < p_y:
+        #         parent[p_y] = p_x
+        #     else:
+        #         parent[p_x] = p_y
 
         i = answer = 0
         while i < len(ranges):
@@ -27,7 +27,7 @@ class Solution:
             par = i
             i += 1
             while i < len(ranges) and e >= ranges[i][0]:
-                union(par, i)
+                # union(par, i)
                 if e < ranges[i][1]:
                     e = ranges[i][1]
                 i += 1
